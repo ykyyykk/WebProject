21fe804684cde01a7ab4b8f472727e4cee45a6e2
AACCS\louis <louise87276@gmail.com>
2024-09-12 22:18:51 +0800

    a

--- src/components/SwiperComponent.vue
+++ src/components/SwiperComponent.vue
@@ -62,3 +62,144 @@ export default {
   },
 };
 </script>
+
+<!-- Carousel.js -->
+<!-- let images = [
+  "",
+  "image/1.jpg",
+  "image/2.jpg",
+  "image/3.jpg",
+  "image/4.jpg",
+  "image/5.jpg",
+]; /* ImageURL */
+
+var currentIndex = 0;
+// 5秒
+var changeDuration = 3000;
+var animation;
+var lastImgElement;
+var player;
+
+function animationInit() {
+  animation = anime({
+    targets: "#fill",
+    width: "100%",
+    easing: "linear",
+    duration: changeDuration,
+    autoplay: false,
+    complete: () => {
+      $("#fill").css("width", "0%");
+      nextImage();
+    },
+  });
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("trailer", {
+    events: {
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+// 不知道為什麼 有的時候 沒有""會Error
+$(document).ready(() => {
+  checkIndexShowImage();
+  ButtonInit();
+  animationInit();
+  onYouTubeIframeAPIReady();
+  animation.play();
+
+  $("#big_img").on("mouseenter", () => {
+    animation.pause();
+  });
+
+  $("#big_img").on("mouseleave", () => {
+    animation.play();
+  });
+});
+
+function updateImage(carouselIndex) {
+  // 避免手動選第三張 在按下一張的index不對
+  currentIndex = carouselIndex;
+  // console.log(currentIndex);
+  checkIndexShowImage();
+
+  $("#big_img").attr("src", images[currentIndex]);
+  // ChangeFoucusBorder(currentIndex);
+
+  animation.reset();
+  animation.play();
+}
+
+function nextImage() {
+  currentIndex++;
+  currentIndex = currentIndex >= images.length ? 0 : currentIndex;
+  updateImage(currentIndex);
+}
+
+function previousImage() {
+  currentIndex--;
+  currentIndex = currentIndex < 0 ? images.length - 1 : currentIndex;
+  updateImage(currentIndex);
+}
+
+function ButtonInit() {
+  $("#nextPageBtn").on("click", () => {
+    nextImage();
+  });
+  $("#previousPageBtn").on("click", () => {
+    previousImage();
+  });
+}
+
+function checkIndexShowImage() {
+  if (currentIndex != 0) {
+    $("#trailer").css("display", "none");
+    $("#big_img").css("display", "block");
+  } else {
+    $("#trailer").css("display", "block");
+    $("#big_img").css("display", "none");
+  }
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    animation.pause();
+    return;
+  }
+  animation.play();
+}
+
+function ChangeFoucusBorder(currentIndex) {
+  $(`#carousel${currentIndex}`).addClass("border border-2 border-primary");
+  if ($(lastImgElement) != undefined) {
+    $(lastImgElement).removeClass("border border-2 border-primary");
+  }
+  lastImgElement = $(`#carousel${currentIndex}`);
+} -->
+
+<!-- ScrollButton.js -->
+<!-- var scroll_to_top_btn;
+var scroll_to_bottom_btn;
+
+document.addEventListener("DOMContentLoaded", () => {
+  scroll_to_top_btn = document.getElementById("ScrollToTop");
+  scroll_to_bottom_btn = document.getElementById("ScrollToBottom");
+
+  scroll_to_top_btn.addEventListener("click", () => {
+    console.log("scroll to top");
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" /* 平滑捲動 */,
+    });
+  });
+
+  scroll_to_bottom_btn.addEventListener("click", () => {
+    console.log("scroll to bottom");
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  });
+}); -->
