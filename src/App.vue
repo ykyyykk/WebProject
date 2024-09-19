69ed30e1b6d4f0913638f3665315a49170b28a5f
AACCS\louis <louise87276@gmail.com>
2024-09-19 20:22:44 +0800

    測試

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
 <template>
   <!-- 不要嘗試在這邊 :items="items" 改用vuex的getter 或 computed 來抓items -->
   <router-view />
+  <FooterComponent />
 </template>
+
+<script>
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  components: { FooterComponent },
+};
+</script>
 
 <style>
 html {
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,20 +1,21 @@
 <template>
-  <footer class="bg-primary m-0">
-    <div>
-      <h3>客戶服務</h3>
-      <ul>
-        <li>
-          <a class="text-black text-decoration-none" href="">聯繫我們</a>
-        </li>
-        <li><a class="text-black text-decoration-none" href="">常見QA</a></li>
-        <li>
-          <a class="text-black text-decoration-none" href="">退貨/物流政策</a>
-        </li>
-        <li>
-          <a class="text-black text-decoration-none" href="">隱私權政策</a>
-        </li>
-      </ul>
+  <footer
+    class="text-bg-secondary p-3 d-flex justify-content-start w-100 h-100 border border-black"
+  >
+    <div class="col-6">
+      <h3 class="fw-bolder">客戶服務</h3>
+      <p><a class="text-white text-decoration-none" href="">常見QA</a></p>
+      <p>
+        <a class="text-white text-decoration-none" href="">退貨/物流政策</a>
+      </p>
+      <p><a class="text-white text-decoration-none" href="">隱私權政策</a></p>
     </div>
-    <div><h3>關於我們</h3></div>
+    <div>
+      <h3 class="fw-bolder">聯繫我們</h3>
+      <p>綠界科技的店</p>
+      <p><i class="fa-solid fa-envelope mx-2"></i>louise87276@gmail.com</p>
+      <p><i class="fa-solid fa-phone mx-2"></i>+886 0954074430</p>
+      <p><i class="fa-brands fa-line mx-2"></i>0954074430</p>
+    </div>
   </footer>
 </template>
