3fcf9582bbc0fbdd3a6d5cb4b87a16e42dac3f2c
汪世傑 <louise87276@gmail.com>
2024-09-25 15:41:40 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+// import { resolve } from "path";
 // https://vitejs.dev/config/
 
 // MPA會在這邊做page設定 有點像是router.js做的事
 export default defineConfig({
   plugins: [vue()],
+
   // 嘗試上傳到github.pages 失敗 應該跟讀不到server 資料庫有關
   // base: "/WebProject/",
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "index.html"),
+  //     },
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     vue: "https://unpkg.com/vue@3/dist/vue.esm-browser.js",
+  //   },
+  // },
 });
