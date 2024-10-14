8cad2c15fbf16cf0311de4ae1c631dee9ab20529
汪世傑 <louise87276@gmail.com>
2024-10-14 14:58:35 +0800

    新增 login 單元測試

--- test/login.js
+++ test/login.js
@@ -1,84 +1,21 @@
-// import { shallowMount } from "@vue/test-utils";
-// import { createStore } from "vuex";
-// import axios from "axios";
-// import Login from "../src/pages/Login.vue";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.production" });
 
-// jest.mock("axios");
+describe("Login.vue", () => {
+  // 不要在 () 裡面加上 done 因為是async
+  it("呼叫 /login API 成功", async () => {
+    const response = await axios.post(
+      `${process.env.VITE_API_BASE_URL}/api/login`,
+      {
+        email: "e",
+        password: "p",
+      }
+    );
 
-// describe("Login.vue", () => {
-//   let actions;
-//   let store;
-
-//   beforeEach(() => {
-//     actions = {
-//       SetLogin: jest.fn(),
-//     };
-//     store = createStore({
-//       actions,
-//     });
-//   });
-
-//   it("calls login API and handles successful response", async () => {
-//     const wrapper = shallowMount(Login, {
-//       global: {
-//         plugins: [store],
-//       },
-//       data() {
-//         return {
-//           email: "e",
-//           password: "p",
-//         };
-//       },
-//     });
-
-//     const mockResponse = {
-//       data: {
-//         success: true,
-//         user: { id: 1, name: "Test User" },
-//       },
-//     };
-//     axios.post.mockResolvedValue(mockResponse);
-
-//     await wrapper.vm.Login();
-
-//     expect(axios.post).toHaveBeenCalledWith(expect.any(String), {
-//       email: "e",
-//       password: "p",
-//     });
-//     expect(actions.SetLogin).toHaveBeenCalledWith(expect.anything(), {
-//       user: mockResponse.data.user,
-//     });
-//     expect(localStorage.setItem).toHaveBeenCalledWith(
-//       "user",
-//       JSON.stringify(mockResponse.data.user)
-//     );
-//   });
-
-//   it("handles login failure", async () => {
-//     const wrapper = shallowMount(Login, {
-//       global: {
-//         plugins: [store],
-//       },
-//       data() {
-//         return {
-//           email: "test@example.com",
-//           password: "wrongpassword",
-//         };
-//       },
-//     });
-
-//     axios.post.mockRejectedValue(new Error("Login failed"));
-
-//     const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
-
-//     await wrapper.vm.Login();
-
-//     expect(axios.post).toHaveBeenCalledWith(expect.any(String), {
-//       email: "test@example.com",
-//       password: "wrongpassword",
-//     });
-//     expect(alertMock).toHaveBeenCalledWith(
-//       expect.stringContaining("帳號或密碼錯誤")
-//     );
-//   });
-// });
+    response.data.should.deepEqual({
+      success: true,
+      user: { id: 6, name: "e", phoneNumber: "p", email: "e", password: "p" },
+    });
+  });
+});
