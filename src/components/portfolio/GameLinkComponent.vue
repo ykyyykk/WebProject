8425f8dbcd13888e8fd6df212c22cc8ce764946c
汪世傑 <louise@mosji.co>
2026-04-13 18:12:33 +0800

    feat: 修正平台文字 移除simmer

--- src/components/portfolio/GameLinkComponent.vue
+++ src/components/portfolio/GameLinkComponent.vue
@@ -2,7 +2,7 @@
   <div v-for="gameLink in gameLinks" :key="gameLink">
     <a class="text-decoration-none btn m-0 d-flex flex-xxl-column flex-xl-column flex-md-column flex-sm-column flex-column align-items-center justify-content-xl-between justify-content-xxl-center col-xl-12 flex-nowrap-4 col-xxl"
       :href="gameLink.href">
-      <p class="text-white fs-3">GooglePlay</p>
+      <p class="text-white fs-3">{{ gameLink.platform }}</p>
       <!-- <img :src="gameLink.src" style="width: 100px; height: 100px" /> -->
     </a>
   </div>
@@ -16,15 +16,18 @@ export default {
         {
           href: "https://store.steampowered.com/app/1483600/_/",
           src: "/static/Steam逢魔之時.png",
+          platform: "Steam"
         },
         {
           href: "https://play.google.com/store/apps/details?id=com.LittleFox.LittleFox&hl=zh_TW",
           src: "/static/GooglePlay逢魔之時.png",
+          platform: "GooglePlay"
         },
-        {
-          href: "https://simmer.io/@louise_bws/littlefox",
-          src: "/static/SimmerQRCode.png",
-        },
+        // {
+        //   href: "https://simmer.io/@louise_bws/littlefox",
+        //   src: "/static/SimmerQRCode.png",
+        //   platform: "Web"
+        // },
       ],
     };
   },
